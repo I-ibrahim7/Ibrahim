25795181882a6d1e0475cdea246a5447cf8e2925
I-ibrahim7 <156250000+I-ibrahim7@users.noreply.github.com>
2024-01-17 21:49:48 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,48 @@
+# sum of two digits
+
+add = (5+6)
+print("answer of addition",(add))
+
+#division of two digits
+
+divide = (25/5)
+print("answer if division is",(divide))
+
+#Finding the area of triangle
+#formula of triangle is
+#1/2(lxh)
+
+length_of_triangle = 10
+height_of_triangle = 15
+print("the area of triangle is ",(length_of_triangle)*(height_of_triangle)/2)
+
+#changing celsius to fahrenheit
+
+#formula: c*(9/5)+32
+
+temperature_given_in_celcius = (37)
+print(temperature_given_in_celcius*(9/5)+32)
+
+#data types (int)
+
+a = int(input("enter number "))
+print("the data type of given num is ",type(a))
+
+#data types (float)
+b = float(input("enter number"))
+print("the data type of given number is ",type(b))
+
+#data types (str)
+
+c = str(input("enter value"))
+print("the data type og given value is ",type(c))
+
+#data types (bool)
+
+d = bool(input("enter value"))
+print("the data type of given value is ",type(d))
+
+# name: ibrahim 
+#num: 03323376179
+#email: mibbu321@gmail.com
+
